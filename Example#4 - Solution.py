7270da6ab58454eb79f8636cdcdbcf87c8995c90
mschen-sta <cherrie.chen@ycdsb.ca>
2024-05-02 15:00:17 -0400

    Updated with lesson material

--- Example#4 - Solution.py
+++ Example#4 - Solution.py
@@ -1,2 +1,17 @@
 # Example 4 - Guessing Game - Correct Solution
 
+import random
+
+num = random.randrange(1, 101)
+
+guess = int(input("Enter a guess: "))
+
+while guess != num:
+  if guess > num:
+    print("Guess is too high. Guess again.")
+  else:
+    print("Guess is too low. Guess again.")
+    
+  guess = int(input("Enter a guess: "))
+
+print("Congratulations! You guessed correct!")
